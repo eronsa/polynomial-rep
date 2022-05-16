fa618c129652ea1dda482288dacaa13e1396ba8f
TerribleAspect <81832477+TerribleAspect@users.noreply.github.com>
2022-05-17 01:14:24 +0800

    additional comments

--- Example.py
+++ Example.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
 
     p1 = Polynomial(1, 2, 3, 4)  # x³ + 2x² + 3x + 4
     p2 = Polynomial(2, 3, 2)  # 2x² + 3x + 2
-    print(p1.__doc__)
-    print(p1 + p2)  # 4x³ + 6x² + 6x + 1
+    print(p1 + p2)  # outputs 4x³ + 6x² + 6x + 1
